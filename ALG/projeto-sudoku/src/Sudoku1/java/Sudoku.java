50e9216bacf1d8579002d2c1ef6c97630f14351b
Ana Carolina Moura <caroltorresact@gmail.com>
2025-12-03 02:26:48 +0000

    add new comments in sudoku

--- ALG/projeto-sudoku/src/Sudoku1/java/Sudoku.java
+++ ALG/projeto-sudoku/src/Sudoku1/java/Sudoku.java
@@ -6,30 +6,40 @@ public class Sudoku {
     public static void main(String[] args) {
         System.out.println("******BEM VINDO AO SUDOKU *******");
         Scanner scanner = new Scanner(System.in);
-        int d = 9;
-        boolean continuar = true;
+
+        int d = 9; // PORQUE O É O MAIOR NUMERO e JÁ ESTÁ IMPLÍCITO NO EXEC
+        boolean continuar = true; // PRECISO QUE O CÓDIGO CONTINUE ATÉ TODAS AS CONDIÇÕES SEREM VERDADES
 
         while (continuar) {
-            System.out.print("Digite um número com 9 digitos, de 1 a 9: ");
-            long num = scanner.nextLong();
+            System.out.print("Digite um número inteiro com 9 digitos, de 1 a 9: ");
+            long num = scanner.nextLong(); // SCANNER DO NUMERO INTEIRO LONG DIGITADO PELO USUÁRIO
 
-            int resultado = 0;
+            int resultado = 0; // RESULTADO TEM QUE COMEÇAR NA POSIÇÃO 0
 
             // 1. num é positivo e tem 9 dígitos
-            String numStr = String.valueOf(num);
-            int quantidadeDigitos = numStr.length();
+            String numStr = String.valueOf(num); // Converte qualquer valor (int, long, float, objeto) em String // CONVERTI PARA STRING PARA ANALISAR OS dígitos como caracteres
+            System.out.println(numStr.length());  // quantos caracteres existem dentro da String
+            // Length() retorna a quantidade de caracteres de uma string
+            // USEI METODO DE STRING lenght() para contar os caracteres da String
 
+
+            int quantidadeDigitos = numStr.length(); // quantos caracteres foram digitados
+
+            // 1. num é positivo e tem 9 dígitos
             if (num <= 0 || quantidadeDigitos != d) {
                 resultado = 1; // guardar cada resultado em variáveis "resultados" para depois mostrar igual o ex pede a exibiçao dos resultados
             } else {
                 // Guardar cada dígito num array
-                int[] digitos = new int[d];
+                int[] digitos = new int[d]; // Cria array igual a d(9) de modo que nunca ultrapasse as 9 posiçoes
 
                 for (int i = 0; i < d; i++) {
-                    digitos[i] = numStr.charAt(i) - '0'; // conversao do char para o valor de int, tabela ASCII
+                    digitos[i] = numStr.charAt(i) - '0'; // acessar o indice com charAt(i) // acessar cada dígito individual
+                    // conversao do char para o valor de int, tabela ASCII
                 }
 
                 // 2. maior dígito é 9 e menor é 1
+
+                // digitos[i] Acesso a array //Lê cada dígito do número
                 int menorDigito = digitos[0];
                 int maiorDigito = digitos[0];
 
@@ -45,9 +55,10 @@ public class Sudoku {
                 if (menorDigito != 1 || maiorDigito != d) {
                     resultado = 2;
                 } else {
+
                     // 3. a soma dos dígitos de num é igual a 1+2+...+d e o seu o produto é igual a 1x2x...xd
                     int soma = 0;
-                    long produto = 1;
+                    long produto = 1; // produto como long para não ultrapassar limite de int e SE TIVER 10 DIGITOS VAI QUEBRAR O
 
                     for (int i = 0; i < d; i++) {
                         soma = soma + digitos[i];
